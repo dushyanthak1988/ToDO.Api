08080e0fba7c3b1ec3aad9c620bf681beb131f65
Dushyantha Kalehewatta <Ulindu.Dushyantha@softlogiclife.lk>
2020-09-05 16:17:30 +0530

    Add project files.

---
+++ ToDO.Api/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ToDo.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Entities.ToDo> ToDos { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
